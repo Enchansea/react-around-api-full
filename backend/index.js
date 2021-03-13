a4d251be7e5de5f83bbfafd228e64567bfd1f4cd
Vennessa Chow <vennessa.chow@gmail.com>
2021-03-13 11:38:40 -0800

    wrking on commit

--- backend/index.js
+++ backend/index.js
@@ -46,10 +46,14 @@ app.post('/signin', celebrate({
     password: Joi.string().required,
   }),
 }), login);
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
     email: Joi.string().required().email(),
-    password: Joi.string().required,
+    password: Joi.string().required(),
   }),
 }), createUser);
 

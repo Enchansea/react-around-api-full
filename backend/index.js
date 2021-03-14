c9cbad68aa21c12035fdb17c4ca9b15c436694fb
Vennessa Chow <vennessa.chow@gmail.com>
2021-03-13 17:19:30 -0800

    fixed joi error

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ app.use(limiter);
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required,
+    password: Joi.string().required(),
   }),
 }), login);
 
